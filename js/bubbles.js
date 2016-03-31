f54833697e58e6bb4c856d793a8f01d1b61f13ec
Flavio Coutinho <fegemo@gmail.com>
2016-03-31 11:10:24 -0300

    Improves bubbles.js
    Now it disposes of the bubbles that reach the top
    Also the bubbles have variable size

--- js/bubbles.js
+++ js/bubbles.js
@@ -3,14 +3,15 @@ function loadImage(path) {
     img.src = path;
     return img;
 }
-
+var bubbleEl = loadImage('images/bolha.png');
 function instantiateBubble() {
-    var newBubbleEl = loadImage('images/bolha.png');
+    var newBubbleEl = bubbleEl.cloneNode();
     newBubbleEl.style.position = 'fixed';
     newBubbleEl.style.transition = 'all 5s linear';
     newBubbleEl.style.bottom = '-50px';
     newBubbleEl.style.left = Math.random() * window.innerWidth + 'px';
     newBubbleEl.style.opacity = 1;
+	newBubbleEl.style.transform = 'scale(' + (Math.random() / 2 + 0.5) + ')';
     newBubbleEl.style.zIndex = 10000;
 
     document.body.appendChild(newBubbleEl);
@@ -18,8 +19,15 @@ function instantiateBubble() {
         newBubbleEl.style.opacity = .5;
         newBubbleEl.style.bottom = window.innerHeight + 'px';
     }, 100);
+    (function(oldBubbleEl) {
+    	function removeElement() {
+    		oldBubbleEl.removeEventListener('transitionend', removeElement);
+    		document.body.removeChild(oldBubbleEl);
+    	}
+		oldBubbleEl.addEventListener('transitionend', removeElement);
+    }(newBubbleEl));
 
-    window.setTimeout(instantiateBubble, 1000 + Math.random()*5000)
+    window.setTimeout(instantiateBubble, 7000 + Math.random()*5000)
 }
 
 
